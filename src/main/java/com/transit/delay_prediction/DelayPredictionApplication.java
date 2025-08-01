68a640f3d7ba2babddb69db39bb42f7603bc6ca2
raas21 <ramrajs2102@gmail.com>
2025-08-01 23:53:05 +0530

    Initial Spring Boot setup with Datasource config

---
+++ src/main/java/com/transit/delay_prediction/DelayPredictionApplication.java
@@ -0,0 +1,13 @@
+package com.transit.delay_prediction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelayPredictionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DelayPredictionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/transit/delay_prediction/DelayPredictionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transit.delay_prediction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelayPredictionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
